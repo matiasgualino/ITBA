104350d1d45b2374f7aebeb9a8e87b3fe4a0a5a1
Alejandro Bezdjian <alebezdjian@Alejandros-MacBook-Pro.local>
2015-05-18 11:52:29 -0300

    Funcional

--- Programacion funcional/practica5.hs
+++ Programacion funcional/practica5.hs
@@ -45,3 +45,64 @@ moreThan :: [[a]] -> Int -> [[a]]
 moreThan [] n = []
 moreThan (l:ls) n = if ((length l) > n) then (l : moreThan ls n) else (moreThan ls n)
 
+--Ejercicio 9
+getodds :: [[Int]] -> [Int]
+getodds [] = []
+getodds (x:xs) = (getoddsaux x) ++ (getodds xs)
+
+getoddsaux :: [Int] -> [Int]
+getoddsaux [] = []
+getoddsaux (x:xs) = if (mod x 2 == 0) then (getoddsaux xs) else (x : getoddsaux xs)
+
+--Ejercicio 10
+data DigBin = Cero | Uno deriving (Show)
+
+sumBin :: [DigBin] -> [DigBin] -> [DigBin]
+sumBin l1 l2 = reverse (sumBinAux (reverse l1) (reverse l2) Cero)
+
+sumBinAux :: [DigBin] -> [DigBin] -> DigBin -> [DigBin]
+sumBinAux [] [] Uno = Uno : []
+sumBinAux [] [] Cero = []
+sumBinAux [] (Uno:bs) Uno = Cero : sumBinAux [] bs Uno
+sumBinAux [] (Cero:bs) Uno = Uno : sumBinAux [] bs Cero
+sumBinAux [] (Uno:bs) Cero = Uno : sumBinAux [] bs Cero
+sumBinAux [] (Cero:bs) Cero = Cero : sumBinAux [] bs Cero
+sumBinAux (Uno:bs) [] Uno = Cero : sumBinAux bs [] Uno
+sumBinAux (Cero:bs) [] Uno = Uno : sumBinAux bs [] Cero
+sumBinAux (Uno:bs) [] Cero = Uno : sumBinAux bs [] Cero
+sumBinAux (Cero:bs) [] Cero = Cero : sumBinAux bs [] Cero
+sumBinAux (Cero:lb1) (Cero:lb2) Cero = Cero : sumBinAux lb1 lb2 Cero
+sumBinAux (Cero:lb1) (Cero:lb2) Uno = Uno : sumBinAux lb1 lb2 Cero
+sumBinAux (Uno:lb1) (Cero:lb2) Cero = Uno : sumBinAux lb1 lb2 Cero
+sumBinAux (Uno:lb1) (Cero:lb2) Uno = Cero : sumBinAux lb1 lb2 Uno
+sumBinAux (Cero:lb1) (Uno:lb2) Cero = Uno : sumBinAux lb1 lb2 Cero
+sumBinAux (Cero:lb1) (Uno:lb2) Uno = Cero : sumBinAux lb1 lb2 Uno
+sumBinAux (Uno:lb1) (Uno:lb2) Cero = Cero : sumBinAux lb1 lb2 Uno
+sumBinAux (Uno:lb1) (Uno:lb2) Uno = Uno : sumBinAux lb1 lb2 Uno
+
+multBin :: [DigBin] -> [DigBin] -> [DigBin]
+multBin l1 l2 = reverse(multBinAux (reverse l1) (reverse l2) [Cero])
+
+multBinAux :: [DigBin] -> [DigBin] -> [DigBin] -> [DigBin]
+multBinAux [Uno] l1 ans = sumBinAux ans l1 Cero
+multBinAux [Cero] l2 ans = ans
+multBinAux (Uno:l1s) (l2:l2s) ans = multBinAux l1s (Cero:l2:l2s) (sumBinAux ans (l2:l2s) Cero)
+multBinAux (Cero:l1s) (l2:l2s) ans = multBinAux l1s (Cero:l2:l2s) ans
+
+binToInt :: [DigBin] -> Int
+binToInt l1 = binToIntAux (reverse l1) 0 0
+
+binToIntAux :: [DigBin] -> Int -> Int -> Int
+binToIntAux (Uno:ls) 0 ans = binToIntAux ls 2 1
+binToIntAux (Uno:ls) b ans = binToIntAux ls (b*2) (ans+b)
+binToIntAux (Cero:ls) 0 ans = binToIntAux ls 2 ans
+binToIntAux (Cero:ls) b ans = binToIntAux ls (b*2) ans
+binToIntAux [] b ans = ans
+
+--intToBin :: Int -> [DigBin]
+intToBin 0 = [Cero]
+intToBin n = intToBinAux n (floor(logBase 2 n))
+
+--intToBinAux :: Int -> Integral -> [DigBin]
+intToBinAux n (-1) = []
+intToBinAux n p = if (n - 2^p >= 0) then (Uno : intToBinAux (n-2^p) (p-1)) else (Cero : intToBinAux n (p-1))
---
+++ Programacion funcional/practica7.hs
@@ -0,0 +1,104 @@
+import Data.Char
+{-
+filter :: (a -> Bool) -> [a] -> [a]
+filter f [] = []
+filter f (x:xs) = if f x
+                  then x:(filter f xs)
+                  else filter f xs
+
+map :: (a -> b) -> [a] -> [b]
+map f [] = []
+map f (x:xs) = f x : map f xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f z [] = z
+foldr f z (x:xs) = f x (foldr f z xs)
+-}
+
+--Ejercicio 1
+sumlist :: Num a => [a] -> a
+sumlist = foldr (\n b -> n + b) 0
+
+anyinlist :: [Bool] -> Bool
+anyinlist = foldr (\x b -> x || b) False
+
+allinlist :: [Bool] -> Bool
+allinlist = foldr (\x b -> x && b) True
+
+codes :: [Char] -> [Int]
+codes = map ord
+
+remainders :: Int -> [Int] -> [Int]
+remainders n = map (\x -> mod x n)
+
+squares :: [Int] -> [Int]
+squares = map (\x -> x*x)
+
+lengths :: [[a]] -> [Int]
+lengths = map length
+
+order :: [(Int,Int)] -> [(Int,Int)]
+order = filter (\(a,b) -> a<3*b)
+
+pairs :: [Int] -> [Int]
+pairs = filter (\x -> mod x 2 == 0)
+
+chars :: [Char] -> [Char]
+chars = filter isAlpha
+
+moreThan :: Int -> [[a]] -> [[a]]
+moreThan n = filter (\x -> length x > n)
+
+--Ejercicio 2
+pal :: [Char] -> Bool
+pal s = (s == reverse s)
+
+hs :: [[Char]] -> Int
+hs = foldr (\(x:xs) b -> if (x=='h' || x=='H') then 1+b else b) 0
+
+avgLength :: Fractional b => [[a]] -> b
+avgLength []     = 0
+avgLength (x:xs) = fromIntegral (sum (lengths (x:xs))) / fromIntegral (length (x:xs))
+
+adjacents :: [a] -> [(a,a)]
+adjacents [] = []
+adjacents [_] = []
+adjacents (x:y:zs) = (x,y) : adjacents (y:zs)
+
+diffAdj :: [Int] -> [(Int,Int)]
+diffAdj [] = []
+diffAdj [_] = []
+diffAdj (x:y:zs) = if (mod (x-y) 2 == 0) then (x,y):diffAdj(y:zs) else diffAdj (y:zs)
+
+remDups :: [Int] -> [Int]
+remDups [] = []
+remDups [a] = [a]
+remDups (x:y:zs) = if (x==y) then remDups(y:zs) else x:remDups(y:zs)
+
+---------------------------GUIA 4-------------------------------
+prime 1 = False
+prime 2 = True
+prime x = primerec 2 x
+
+primerec y x = if (mod x y == 0)
+					then False
+					else if (y < floor (sqrt(fromInteger x)))
+							then True && (primerec (y+1) x)
+							else True
+----------------------------------------------------------------
+primes n = if(n>0) then primesRec (n-1) 2 [2] else []
+primesRec n ult ans = if(n>0)
+						then (if prime(ult+1) 
+									then primesRec (n-1) (ult+1) (ans++[ult+1]) 
+									else primesRec n (ult+1) ans) 
+						else ans
+
+--Ejercicio 3
+f :: [a] -> [a]
+f = foldr (:) []
+--Devuelve la misma lista
+f' (x:xs) = (x:xs)
+
+--Ejercicio 4
+filtro f list = filtroAux list (map f list)
+filtroAux (l1:l1s) (l2:l2s) = if l2 then [l1] ++ (filtroAux l1s l2s) else [] ++ (filtroAux l1s l2s)
